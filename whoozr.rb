50036eda2bbcb3ce83a2f3f504e0bea0ddc7e8f9
Kurt Sussman <kls@merlot.com>
2013-02-12 16:45:50 -0800

    still needs tests, but tons more functional

--- whoozr.rb
+++ whoozr.rb
@@ -1,23 +1,29 @@
-require 'rubygems'
+require 'trollop'
 require 'whois'
+
+opts = Trollop::options do
+  opt :tlds,
+    "Top Level Domains, comma separated (default: me,ag,org,us)",
+    :type => :string,
+    :default => "me,ag,org,us"
+  opt :file,
+    "Input file for word list (use stdin if not specified)",
+    :type => :string
+  banner "look for words with TLDs at the end, see if the domain is available"
+end
+
+Trollop::die :file, "must exist" unless File.exist?(opts[:file])
+
+tld_regex = "(" + opts[:tlds].split(',').join('$|') + "$)"
 
 c = Whois::Client.new
 
-ARGV.each do |dom|
+words = File.read(opts[:file]).split
+
+words.each do |dom|
   check = String.new(dom)
-  if !(i = check.index(/me$/)).nil? then 
-    check = check.insert(i, '.') 
-  end
-  if !(i = check.index(/ag$/)).nil? then 
-    check = check.insert(i, '.') 
-  end
-  if !(i = check.index(/org$/)).nil? then 
-    check = check.insert(i, '.') 
-  end
-  if !(i = check.index(/us$/)).nil? then 
-    check = check.insert(i, '.') 
-  end
-  if check.index('.') && c.query(check).available? then
-    puts check + " is available"
+  if !(i = check.index(/#{tld_regex}/)).nil? then
+    check = check.insert(i, '.')
+    puts check + " is available" if c.query(check).available?
   end
 end
