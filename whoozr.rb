e0a9136a45dd5943a725bcabcf0cbcb7d97b2fbf
Kurt Sussman <kls@merlot.com>
2013-03-12 13:48:57 -0700

    refactor to allow adding TLDs as well as finding them at the end of the words

--- whoozr.rb
+++ whoozr.rb
@@ -1,11 +1,49 @@
 require 'trollop'
 require 'whois'
 
+class DomainList
+  attr_reader :words, :tlds, :concat, :domains
+  def initialize(args)
+    @words  = args[:words]
+    @tlds   = args[:tlds]
+    @domains = []
+    args[:concat] ? add_tlds : find_tlds
+    p @domains
+  end
+
+  def add_tlds
+    tld_list = @tlds.split(',')
+    @words.each do |word|
+      base = String.new(word)
+      tld_list.each do |tld|
+        @domains << base + '.' + tld
+        if base[-1,1] == 's' # check non-plural word also?
+          @domains << base[0,base.size-1] + '.' + tld
+        end
+      end
+    end
+  end
+
+  def find_tlds
+    tld_regex = "(" + @tlds.split(',').join('$|') + "$)"
+    @words.each do |word|
+      check = String.new(word)
+      if !(i = check.index(/#{tld_regex}/)).nil? then
+        @domains << check.insert(i, '.')
+      end
+    end
+  end
+
+end
+
 opts = Trollop::options do
   opt :tlds,
     "Top Level Domains, comma separated (default: me,ag,org,us)",
     :type => :string,
     :default => "me,ag,org,us"
+  opt :concat,
+    "Add the listed TLDs to the end of the word before checking.
+    If false or not specified, look for TLDs included at the end of the word"
   opt :file,
     "Input file for word list (use stdin if not specified)",
     :type => :string
@@ -14,16 +52,15 @@ end
 
 Trollop::die :file, "must exist" unless File.exist?(opts[:file])
 
-tld_regex = "(" + opts[:tlds].split(',').join('$|') + "$)"
+potentials = DomainList.new(
+  :words => File.read(opts[:file]).split,
+  :tlds => opts[:tlds],
+  :concat => opts[:concat]
+)
 
 c = Whois::Client.new
 
-words = File.read(opts[:file]).split
-
-words.each do |dom|
-  check = String.new(dom)
-  if !(i = check.index(/#{tld_regex}/)).nil? then
-    check = check.insert(i, '.')
-    puts check + " is available" if c.query(check).available?
-  end
+potentials.domains.each do |dom|
+  puts dom + " is available" if c.query(dom).available?
 end
+
