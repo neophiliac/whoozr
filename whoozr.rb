d18a44073c8a806844769c28fd6bf9fae8627fa7
Kurt Sussman <kls@merlot.com>
2013-04-23 20:04:51 -0700

    Make file parm required, rescue from common exceptions

--- whoozr.rb
+++ whoozr.rb
@@ -12,7 +12,8 @@ opts = Trollop::options do
     If false or not specified, look for TLDs included at the end of the word"
   opt :file,
     "Input file for word list (use stdin if not specified)",
-    :type => :string
+    :type => :string,
+    :required => true
   banner "look for words with TLDs at the end, see if the domain is available"
 end
 
@@ -24,9 +25,26 @@ potentials = DomainList.new(
   :concat => opts[:concat]
 )
 
-c = Whois::Client.new
+c = Whois::Client.new(timeout: 20)
 
 potentials.domains.each do |dom|
-  puts dom + " is available" if c.query(dom).available?
+  attempts = 0
+  done = false
+  while !done and attempts < 3 do
+    begin
+      attempts += 1
+      puts dom + " is available" if c.lookup(dom).available?
+      done = true
+    rescue Whois::ConnectionError
+      #puts "ConnectionError, retrying"
+      retry
+    rescue Whois::ResponseIsThrottled
+      #puts "ResponseIsThrottle, retrying"
+      retry
+    rescue Timeout::Error
+      #puts "ResponseIsThrottle, retrying"
+      retry
+    end
+  end
 end
 
